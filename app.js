b0a6f06115ca0d5ffba302e4e3e6937a22da3eae
vinaykashyap1996 <vinay.kashyap234@gmail.com>
2021-12-06 19:09:32 +0100

    graph ql setup and inital queries

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql').graphqlHTTP;
+var { buildSchema } = require('graphql');
+
+var schema = buildSchema(`
+  type RootQuery {
+    events: [String!]!
+  }
+
+  type RootMutation {
+    createEvent(name: String): String 
+  }
+
+  schema {
+      query: RootQuery
+      mutation:RootMutation
+  }  
+`);
+
+
 
 const app = express();
 
 app.use(bodyParser.json())
 
-app.get("/", (req,res,err) =>{
-    res.send("Hello World")
-})
+app.use('/graphql', graphqlHttp({
+    schema: schema,
+    rootValue: {
+        events: () => {
+
+         return ["Gaming" , "playing" , "movie night"]
+
+        } ,
+        createEvent:(args) =>{
+            const EventName = args.name;
+            return EventName;
+        }
+    },
+    graphiql: true,
+  }));
+  
+
 app.listen('3000')
