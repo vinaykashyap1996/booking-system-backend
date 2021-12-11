0dc6262642342109e0d251412682a098ab1d1fa6
vinaykashyap1996 <vinay.kashyap234@gmail.com>
2021-12-11 16:15:58 +0100

    booking events api and moving of the api and schemas from app js to seperate modals

--- app.js
+++ app.js
@@ -1,77 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql').graphqlHTTP;
-var { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
-const Event = require('./models/events')
-var schema = buildSchema(`
-  type Event{
-      _id:ID!
-      title:String!
-      description:String!
-      price:Float!
-      date:String!
+const graphqlSchema = require('./graphql/schema/index');
+const graphqlResolvers = require('./graphql/resolvers/index');
 
-  }
-
-  input EventInput {
-    title:String!
-    description:String!
-    price:Float!
-    date:String!
-  }
-
-  type RootQuery {
-    events: [Event!]!
-  }
-
-  type RootMutation {
-    createEvent(eventInput: EventInput): Event 
-  }
-
-  schema {
-      query: RootQuery
-      mutation:RootMutation
-  }  
-`);
 
 const app = express();
 
 app.use(bodyParser.json())
 
 app.use('/graphql', graphqlHttp({
-    schema: schema,
-    rootValue: {
-        events: () => {
-        return Event.find().then(results =>{
-         return results.map(result =>{
-            return {...result._doc , _id: result.id}
-          })
-        }).catch(err =>{
-          throw err;
-        })
-        } ,
-        createEvent:(args) =>{
-            const event = new Event({
-              title:args.eventInput.title,
-                description:args.eventInput.description,
-                price: +args.eventInput.price,
-                date: new Date(args.eventInput.date)
-            })
-            event.save().then(result =>{
-              return {...result}
-            }).catch(err =>{
-              throw err;
-            })
-            return event;
-        
-    }
-  },
-    graphiql: true,
+  schema: graphqlSchema,
+  rootValue: graphqlResolvers ,
+  graphiql: true,
 
-  }));
+}));
 
-   mongoose.connect(process.env.MONGO_user).then().catch(err =>{
+mongoose.connect(process.env.MONGO_user).then().catch(err => {
   console.log(err)
 })
 app.listen('4000')
