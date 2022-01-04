f1c9977ff32d0b5a5783d4bcf0cf9f52aea0f4f6
vinay kashyap <vinaykashyap@MBP-von-vinay.fritz.box>
2022-01-04 18:58:48 +0100

    fixing bugs

--- app.js
+++ app.js
@@ -1,26 +1,41 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const graphqlHttp = require('express-graphql').graphqlHTTP;
-const mongoose = require('mongoose');
-const graphqlSchema = require('./graphql/schema/index');
-const graphqlResolvers = require('./graphql/resolvers/index');
+const express = require("express");
+const bodyParser = require("body-parser");
+const graphqlHttp = require("express-graphql").graphqlHTTP;
+const mongoose = require("mongoose");
+const graphqlSchema = require("./graphql/schema/index");
+const graphqlResolvers = require("./graphql/resolvers/index");
 
-const isAuth = require('./middleware/is-auth')
+const isAuth = require("./middleware/is-auth");
 
 const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.use(isAuth)
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
-app.use('/graphql', graphqlHttp({
-  schema: graphqlSchema,
-  rootValue: graphqlResolvers ,
-  graphiql: true,
+app.use(isAuth);
 
-}));
+app.use(
+  "/graphql",
+  graphqlHttp({
+    schema: graphqlSchema,
+    rootValue: graphqlResolvers,
+    graphiql: true,
+  })
+);
 
-mongoose.connect(process.env.MONGO_user).then().catch(err => {
-  console.log(err)
-})
-app.listen('4000')
+mongoose
+  .connect(process.env.MONGO_user)
+  .then()
+  .catch((err) => {
+    console.log(err);
+  });
+app.listen("8000");
